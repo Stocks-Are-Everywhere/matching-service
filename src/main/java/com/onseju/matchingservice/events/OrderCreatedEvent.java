e1dcf118f5b4f6307c4b3aab456fc12018f5717b
HminiL <hmin10580@gmail.com>
2025-03-27 13:38:24 +0900

    feat: add gitignore file

---
+++ src/main/java/com/onseju/matchingservice/events/OrderCreatedEvent.java
@@ -0,0 +1,22 @@
+package com.onseju.matchingservice.events;
+
+import java.math.BigDecimal;
+
+import com.onseju.matchingservice.domain.OrderStatus;
+import com.onseju.matchingservice.domain.Type;
+
+import lombok.Builder;
+
+@Builder
+public record OrderCreatedEvent(
+		Long id,
+		String companyCode,
+		Type type,
+		OrderStatus status,
+		BigDecimal totalQuantity,
+		BigDecimal remainingQuantity,
+		BigDecimal price,
+		Long timestamp,
+		Long accountId
+) {
+}
